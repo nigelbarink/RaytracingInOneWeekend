35c69dbee61df5d79fd34126ae70bd6391bf1289
Nigel <nigelbarink@hotmail.com>
2023-03-05 12:02:51 +0100

    Adding material abstractio plus metal reflective material

--- src/color.h
+++ src/color.h
@@ -10,9 +10,9 @@ void write_color(std::ostream& out, color pixel_color, int samples_per_pixel){
 
     // Divide the color by the number of samples
     auto scale = 1.0 / samples_per_pixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    r = sqrt (scale * r);
+    g = sqrt (scale * g);
+    b = sqrt (scale * b);
 
 
     
